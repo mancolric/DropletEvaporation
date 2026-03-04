237d434260937446fa4795de90aedc5d11c04e0a
Betrix20 <799290@unizar.es>
2026-03-04 10:12:50 +0100

    Version for publication

--- Run_droplet_test.m
+++ Run_droplet_test.m
@@ -9,11 +9,13 @@ addpath(genpath(pwd)); % Add all subfolders of the current directory to the MATL
 
 % ---------------------- MESH AND TIME CONFIGURATION ----------------------
 
-nElems_l          = 25;    % Number of elements in the liquid phase
-nElems_g          = 50;    % Number of elements in the gas phase (first part)
+nElems_l          = 10;    % Number of elements in the liquid phase
+nElems_g          = 12;    % Number of elements in the gas phase
+hmin_l            = 1e-6;  
+hmin_g            = 1e-6; 
 p                 = 5;     % Degree of the polynomial basis functions
 
-Deltat0           = 1e-10;  % Initial time step
+Deltat0           = 1e-6;  % Initial time step
                            % If you get warnings like:
                            %   “Nonlinear solver did not converge at stage 2. Reducing time step”
                            %   or
@@ -21,45 +23,46 @@ Deltat0           = 1e-10;  % Initial time step
                            % you may reduce Deltat0 to avoid them
                            % However, this is optional — time step control will automatically handle it
 
-t_final           = 1000;  % Final simulation time
+t_final           = 1e2;   % Final simulation time
+
                            % Use the actual time you want the simulation to stop at
                            % If you want the simulation to continue until the droplet disappears, 
                            % set a value larger than the expected final time
 
 TimeAdapt         = true;  % Enable adaptive time-stepping (true/false)
-TolT              = 1e-3;  % Tolerance for temporal error (≥1e-3) (used if TimeAdapt = true)
+
+TolT              = 1e-4;  % Tolerance for temporal error (<1e-3) (used if TimeAdapt = true)
 
 % ---------------------------- OUTPUT OPTIONS -----------------------------
 
-PlotRes           = false; % Plot intermediate results (true/false)
+PlotRes           = false;  % Plot intermediate results (true/false)
                            % NOTE: if true, Save must be false
-
-Save              = true;  % Save results to file (true/false)
+Save              = true; % Save results to file (true/false)
                            % NOTE: if true, PlotRes must be false
                            
-n_saved_solutions = 500;   % Number of solution snapshots to save throughout simulation
-n_saves           = 5;    % Number of intermediate "safety saves" during simulation
+n_saved_solutions = 100;   % Number of solution snapshots to save throughout simulation
+n_saves           = 1;    % Number of intermediate "safety saves" during simulation
 
 % ---------------------- INITIAL AND BOUNDARY CONDITIONS ------------------
 
-R_0               = (150e-6)/2; % Initial droplet radius [m]
+R_0               = 0.00025; % Initial droplet radius [m]
 XRad              = 150;        % Domain size (in gas phase) as a multiple of the initial radius
-R_end_percent     = 0.15;       % Final droplet size as a fraction of initial radius (when Save = true)
+R_end_percent     = 0.2;       % Final droplet size as a fraction of initial radius (when Save = true)
 
-T_0               = 310;        % Initial temperature at the center of the droplet [K]
-T_inf             = 1730;       % Ambient (far-field) temperature [K]
+T_0               = 300;        % Initial temperature at the center of the droplet [K]
+T_inf             = 1400;       % Ambient (far-field) temperature [K]
 
 % --------------------------- SPECIES DEFINITION --------------------------
-
-fuel_names        = {'Glicerina', 'Metanol'};     % Fuel components
-mass_fracL        = {0.75, 0.25};                    % Mass fraction of each fuel component (same order as above)
-inert_comps       = {'N2', 'O2', 'CO2', 'H2O'};    % Inert gas species (DO NOT MODIFY)
-mass_fracG        = {0.7248, 0.0, 0.1513, 0.1239}; % Mass fraction of each inert species in the gas phase (same order as above)
+fuel_names        = {'Butanol'};                    % Fuel components
+mass_fracL        = {1.0};                         % Mass fraction of each fuel component (same order as above)
+inert_comps       = {'N2', 'O2', 'CO2', 'H2O'};     % Inert gas species (DO NOT MODIFY)
+mass_fracG        = {0.79, 0.0, 0.0, 0.21};  % Mass fraction of each inert species in the gas phase (same order as above)
 
 % ------------------------ INITIAL SIMULATION CALL ------------------------
 
 % Run the full droplet evaporation simulation from t = 0
-[save_vars] = droplet_test(nElems_l, p, Deltat0, t_final, ...
+
+[save_vars] = droplet_test(nElems_l, nElems_g, hmin_l, hmin_g, p, Deltat0, t_final, ...
     TimeAdapt, TolT, PlotRes, Save, fuel_names, mass_fracL, inert_comps, ...
     mass_fracG, n_saved_solutions, T_0, T_inf, R_0, XRad, R_end_percent, ...
     n_saves);
